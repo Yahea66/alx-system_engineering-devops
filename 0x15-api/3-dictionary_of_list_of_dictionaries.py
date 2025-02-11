76df6e334d51a245cbc1dc4b8504dbc258476c43
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-02-10 19:26:01 -0800

    all records

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import csv
+import json
+import requests
+
+if __name__ == "__main__":
+    url_user = "https://jsonplaceholder.typicode.com/users"
+    url_tasks = "https://jsonplaceholder.typicode.com/todos"
+    users = requests.get(url_user).json()
+
+    dic = {}
+    lis = []
+    for user in users:
+        user_id = user.get("userId")
+        params = {"userId": user_id}
+        tasks = requests.get(url_tasks, params=params).json()
+        for task in tasks:
+            inner = {"username": user.get("username"),
+                     "task": task.get("title"),
+                     "completed": task.get("completed")}
+            lis.append(inner)
+        dic[str(user_id)] = lis
+
+    file_name = "todo_all_employees.json"
+    with open(file_name, mode='w') as f:
+        json.dump(dic, f)
