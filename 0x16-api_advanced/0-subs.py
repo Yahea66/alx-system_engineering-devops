b0afdcfbd610ca902b0424f13719b9b15dcfbe82
Yahya Ahmed <yahya.clash8810@gmail.com>
2025-02-25 11:25:15 -0800

    subs number

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""
+module to retrieve subscribers data
+"""
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers
+    """
+    import requests
+
+    res = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+            headers={"USER-AGENT":"My-User-Agent"},
+            allow_redirects=False)
+
+    if res.status_code >= 300:
+        return 0
+
+    return res.json().get("data").get("subscribers")
